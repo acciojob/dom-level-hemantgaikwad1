d2d31202bb0e3190fe4b3e53abbeec0667ad9290
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-12 16:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let target = document.getElementById("level");
+console.log(getDomLevel(target)); 
+function getDomLevel(element) {
+  let level = 0;
+  while (element) {
+    level++;
+    element = element.parentElement; // move up one parent
+  }
+  return level;
+}
